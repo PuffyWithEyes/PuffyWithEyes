b89956ef922ab149ef11161ef6c051f37e3e6bae
puffy_with_eyes <klimov-danya.04@mail.ru>
2023-09-08 16:00:15 +0300

    Update dwm config

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,25 +2,25 @@
 #include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 16;        /* gaps between windows */
-static const unsigned int snap      = 35;       /* snap pixel */
+static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int snap      = 15;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 16;       /* vertical padding of bar */
-static const int sidepad            = 16;       /* horizontal padding of bar */
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 5;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font, monospace:size=10"};
 static const char dmenufont[]       = { "JetBrainsMono Nerd Font, monospace:size=10"};
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const unsigned int roundcorners = 5;
 static const unsigned int barroundcorners = 5;
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#4a393b";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#e617b6";
-static char selbordercolor[]        = "#f542aa";
-static char selbgcolor[]            = "#4f4746";
+static char normbgcolor[]           = "#00c5ff";
+static char normbordercolor[]       = "#00c5ff";
+static char normfgcolor[]           = "#1e081d";
+static char selfgcolor[]            = "#1e081d";
+static char selbordercolor[]        = "#00c5ff";
+static char selbgcolor[]            = "#0093ff";
 
 static const char *upvol[]      = { "pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[]    = { "pactl", "set-sink-volume", "0", "-5%",     NULL };
@@ -72,6 +72,7 @@ static const Layout layouts[] = {
     { " ",     monocle },
 };
 
+
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
@@ -82,25 +83,27 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-/* static const char *powermenu[] = { "/bin/bash", "-c", "swpb", "p" }; */
 
-static const char *powermenu[] = { "/home/puffy/.config/dwm/scripts/dwm_powermenu.sh", NULL };
+
+static const char *powermenu[]  = { "/home/puffy/.config/dwm/scripts/dwm_powermenu.sh", NULL };
 static const char *searchmenu[] = { "/bin/bash", "-c", "/usr/local/bin/swpb", "s" };
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont };
-static const char *termcmd[]    = { "kitty", NULL };
-static const char *spFirefox[]  = { "firefox", NULL };
-static const char *spEmacs[]    = { "emacsclient", "-c", NULL };
-static const char *spTelegram[] = { "telegram-desktop", NULL };
-
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont };
+static const char *termcmd[]     = { "kitty", NULL };
+static const char *spFirefox[]   = { "firefox", NULL };
+static const char *spEmacs[]     = { "emacsclient", "-c", NULL };
+static const char *spTelegram[]  = { "telegram-desktop", NULL };
+static const char *spFlameshot[] = { "flameshot", "gui", NULL };
+static const char *spThunar[]    = { "thunar", NULL };
+	
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0,			                XF86XK_AudioLowerVolume,   spawn,			{.v = downvol } },
-	{ 0,			                XF86XK_AudioMute,		   spawn,			{.v = mutevol } },
-	{ 0,			                XF86XK_AudioRaiseVolume,   spawn,			{.v = upvol   } },
-    { 0,			                XF86XK_MonBrightnessUp  ,  spawn ,			{.v = light_up}} ,
-	{ 0,			                XF86XK_MonBrightnessDown,  spawn ,			{.v = light_down}},
+    { 0,			                XF86XK_MonBrightnessUp,    spawn ,			{.v = light_up } },
+	{ 0,			                XF86XK_MonBrightnessDown,  spawn ,			{.v = light_down } },
+	{ 0,			                XK_F1,     spawn,		   {.v = downvol } },
+	{ 0,			                XK_F12,	   spawn,		   {.v = mutevol } },
+	{ 0,			                XK_F2,     spawn,		   {.v = upvol } },
     { MODKEY,                       XK_q,      spawn,          {.v = powermenu } },
     { MODKEY|ShiftMask,             XK_u,      spawn,          {.v = searchmenu } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
@@ -123,6 +126,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = spFirefox } },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = spEmacs } },
 	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = spTelegram } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = spFlameshot } },
+	{ MODKEY|ShiftMask,             XK_o,      spawn,          {.v = spThunar } },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
