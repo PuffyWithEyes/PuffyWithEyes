31b04f8dbc8d33e52214e8b0d2985b0824fd9944
Puffy <klimov-danya.04@mail.ru>
2023-10-30 16:25:38 +0300

    Update sway config

--- sway/scripts/lockman.sh
+++ sway/scripts/lockman.sh
@@ -6,6 +6,6 @@ swayidle \
     timeout 150 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock -c 550000 -k
+swaylock -c 550000 -k -i ~/.config/sway/lock.jpg --inside-color f7c1a7ff --ring-color 2d2725ff --line-color 463d39ff --inside-clear-color f7c1a7ff --inside-ver-color f7c1a7ff --ring-ver-color 2d2725ff
 # Kills last background task so idle timer doesn't keep running
 kill %%
